b42601ca75e5b0a9c9c9d09593be8bbb423a6d27
JashanMaan28 <166474619+JashanMaan28@users.noreply.github.com>
2025-12-24 00:55:24 -0800

    Add corn and apple pie items with localization and model files

--- src/main/java/com/jashan/tastytweaks/TastyTweaks.java
+++ src/main/java/com/jashan/tastytweaks/TastyTweaks.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
 public class TastyTweaks implements ModInitializer {
 	public static final String MOD_ID = "tasty-tweaks";
 
@@ -13,12 +15,28 @@ public class TastyTweaks implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final Item CORN = new Item(new Item.Properties()
+		.food(new FoodProperties.Builder()
+			.nutrition(4)
+			.saturationModifier(0.5f)
+			.build()
+	));
+
+	public static final Item APPLE_PIE = new Item(new Item.Properties()
+		.food(new FoodProperties.Builder()
+			.nutrition(8)
+			.saturationModifier(0.9f)
+			.build()
+	));
+
+	
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Enjoy the tasty food!");
 	}
 }
